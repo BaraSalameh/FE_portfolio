a88f437a7dcacfb4b5948eb9bfd7762c0db99306
Bara E. Salameh <barasalameh90@gmail.com>
2025-06-28 01:46:32 +0300

    updating base url for production

--- src/lib/apis/apiClient.ts
+++ src/lib/apis/apiClient.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ?   `${process.env.NEXT_PUBLIC_API_URL}/api`
-        :   '',
+        :   '/api',
     headers: {
         'Content-Type': 'application/json',
     },
