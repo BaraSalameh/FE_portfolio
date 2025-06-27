3b42874ea2bc29e0f49bca8c46d6f98ccfe9a66e
Bara E. Salameh <barasalameh90@gmail.com>
2025-06-27 23:37:32 +0300

    Test proxy

--- src/lib/apis/apiClient.ts
+++ src/lib/apis/apiClient.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    // baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    baseURL: `/api`,
     withCredentials: true, 
 });
 
